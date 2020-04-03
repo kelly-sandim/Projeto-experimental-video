db7ff2d4059e913f525d06ff67c607443668d03f
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-03 10:04:45 -0400

    Criado esqueleto da view SendVideo, assim como sua rota no UserRouter.dart

--- projeto_experimental/lib/src/domain/user/routes/UserRouter.dart
+++ projeto_experimental/lib/src/domain/user/routes/UserRouter.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import '../views/Home.dart';
+import '../views/SendVideo.dart';
 
 class UserRouter {
   BuildContext context;
 
   static var routes = {
-    '/Home': (context) => new Home(),    
+    '/Home': (context) => new Home(),  
+    '/SendVideo': (context) => new SendVideo(),  
   };
 }
---
+++ projeto_experimental/lib/src/domain/user/views/SendVideo.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+import 'dart:async';
+import 'dart:convert';
+
+import '../../../../src/assets/colors/MyColors.dart';
+//import 'package:app_vem_rodar_motorista/src/domain/public/api/PublicApi.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class SendVideo extends StatefulWidget {
+  @override
+  _SendVideoState createState() => _SendVideoState();
+}
+
+class _SendVideoState extends State<SendVideo> {  
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColors.white,
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        leading: Image.asset('./lib/src/assets/images/youtube.png',
+            fit: BoxFit.cover, height: 35),
+        title: Text("YouFace", style: TextStyle(color: MyColors.grey)),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.videocam, size: 40, color: MyColors.grey), onPressed: () {}),          
+        ],
+        backgroundColor: MyColors.white,
+        elevation: 0.0, //para tirar a sombra
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },        
+      ),
+    );
+  }
+}
