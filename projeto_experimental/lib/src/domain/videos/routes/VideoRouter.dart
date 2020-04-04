5b67058bea603ccd55a71cd1fa97f04885c37a21
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-04 09:02:33 -0400

    Criado esqueleto da view RecordVideo assim como rota para ela

--- projeto_experimental/lib/src/domain/videos/routes/VideoRouter.dart
+++ projeto_experimental/lib/src/domain/videos/routes/VideoRouter.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../views/SendVideo.dart';
 import '../views/WaitAnalysis.dart';
 import '../views/Result.dart';
+import '../views/RecordVideo.dart';
 
 class VideoRouter {
   BuildContext context;
@@ -11,5 +12,6 @@ class VideoRouter {
     '/SendVideo': (context) => new SendVideo(),  
     '/WaitAnalysis': (context) => new WaitAnalysis(),
     '/Result': (context) => new Result(),
+    '/RecordVideo': (context) => new RecordVideo(),
   };
 }
---
+++ projeto_experimental/lib/src/domain/videos/views/RecordVideo.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:camera/camera.dart';
+import 'package:video_player/video_player.dart';
+
+import '../../../../src/assets/colors/MyColors.dart';
+//import 'package:app_vem_rodar_motorista/src/domain/public/api/PublicApi.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class RecordVideo extends StatefulWidget {
+  @override
+  _RecordVideoState createState() => _RecordVideoState();
+}
+
+class _RecordVideoState extends State<RecordVideo> {  
+  
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColors.white,      
+      appBar: AppBar(
+        title: Text("Resultados", style: TextStyle(color: MyColors.grey)),        
+        backgroundColor: MyColors.white,
+        elevation: 0.0, //para tirar a sombra
+      ),
+      body: Center(         
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: CircularProgressIndicator(
+                    strokeWidth: 15,
+                    valueColor: new AlwaysStoppedAnimation<Color>(MyColors.primaryColor),                   
+                  ),        
+                  width: 140.0,
+                  height: 140.0,                  
+                ),
+                Padding(padding: EdgeInsets.only(top: 30)),                
+                
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.1,
+                  height: 60,
+                  margin: const EdgeInsets.only(top: 25.0),
+                  child: FlatButton(                    
+                    child: Text(
+                      'Voltar para Home',
+                      style: TextStyle(color: MyColors.white, fontSize: 20.0),
+                    ),
+                    color: MyColors.primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/Home');
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),      
+      ),
+    );
+  }
+}
