fc53e1a1d80f483f76f3714c18cc10b8881d10aa
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-06 14:18:46 -0400

    Botão de logout funciona agora

--- projeto_experimental/lib/src/domain/user/views/Home.dart
+++ projeto_experimental/lib/src/domain/user/views/Home.dart
@@ -16,36 +16,160 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {  
-  int currentIndex = 0;   
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  int currentIndex = 0;
   TabController tabController;
 
-  List<Widget> tabList(BuildContext context) 
-  {
+  List<Widget> tabList(BuildContext context) {
     List<Widget> _tabList = [
-      Container(
-        color: MyColors.white,
-        child: Row(
-          children: <Widget>[
-            
-          ],
-        )
-      ),
       Container(
         color: MyColors.white,
         child: ListView(
           children: <Widget>[
-            new ListTile(
-              title: new Text("Logout", style: TextStyle(color: MyColors.primaryColor, fontSize: 18, fontWeight: FontWeight.bold),),
-              leading: new Icon(Icons.power_settings_new, color: MyColors.primaryColor, size: 30),            
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/Login');
-              },
+            Card(
+                child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.play_circle_filled, color: MyColors.grey, size: 30),
+                        title: Text("Vídeo 1", style: TextStyle(color: MyColors.grey, fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Stack(
+                          alignment: FractionalOffset.bottomRight +
+                              const FractionalOffset(-0.1, -0.1),
+                          children: <Widget>[
+                            Image.asset('./lib/src/assets/images/youtubeReverse.png'),
+                          ]),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            new Divider(),
+            Card(
+                child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.play_circle_filled, color: MyColors.grey, size: 30),
+                        title: Text("Vídeo 1", style: TextStyle(color: MyColors.grey, fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Stack(
+                          alignment: FractionalOffset.bottomRight +
+                              const FractionalOffset(-0.1, -0.1),
+                          children: <Widget>[
+                            Image.asset('./lib/src/assets/images/youtubeReverse.png'),
+                          ]),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+                child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.play_circle_filled, color: MyColors.grey, size: 30),
+                        title: Text("Vídeo 1", style: TextStyle(color: MyColors.grey, fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Stack(
+                          alignment: FractionalOffset.bottomRight +
+                              const FractionalOffset(-0.1, -0.1),
+                          children: <Widget>[
+                            Image.asset('./lib/src/assets/images/youtubeReverse.png'),
+                          ]),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+                child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.play_circle_filled, color: MyColors.grey, size: 30),
+                        title: Text("Vídeo 1", style: TextStyle(color: MyColors.grey, fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Stack(
+                          alignment: FractionalOffset.bottomRight +
+                              const FractionalOffset(-0.1, -0.1),
+                          children: <Widget>[
+                            Image.asset('./lib/src/assets/images/youtubeReverse.png'),
+                          ]),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+                child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.play_circle_filled, color: MyColors.grey, size: 30),
+                        title: Text("Vídeo 1", style: TextStyle(color: MyColors.grey, fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Stack(
+                          alignment: FractionalOffset.bottomRight +
+                              const FractionalOffset(-0.1, -0.1),
+                          children: <Widget>[
+                            Image.asset('./lib/src/assets/images/youtubeReverse.png'),
+                          ]),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+                child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.play_circle_filled, color: MyColors.grey, size: 30),
+                        title: Text("Vídeo 1", style: TextStyle(color: MyColors.grey, fontWeight: FontWeight.bold, fontSize: 20)),
+                      ),
+                      Stack(
+                          alignment: FractionalOffset.bottomRight +
+                              const FractionalOffset(-0.1, -0.1),
+                          children: <Widget>[
+                            Image.asset('./lib/src/assets/images/youtubeReverse.png'),
+                          ]),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
-        )
+        ),
       ),
+      Container(
+          color: MyColors.white,
+          child: ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new Text(
+                  "Logout",
+                  style: TextStyle(
+                      color: MyColors.primaryColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                leading: new Icon(Icons.power_settings_new,
+                    color: MyColors.primaryColor, size: 30),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, '/LoginPage');
+                },
+              ),
+              new Divider(),
+            ],
+          )),
     ];
 
     return _tabList;
@@ -77,10 +201,11 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
             fit: BoxFit.cover, height: 35),
         title: Text("YouFace", style: TextStyle(color: MyColors.grey)),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.videocam, size: 40, color: MyColors.grey), 
-            onPressed: () {              
-              Navigator.pushNamed(context, '/RecordVideo');
-            }),          
+          IconButton(
+              icon: Icon(Icons.videocam, size: 40, color: MyColors.grey),
+              onPressed: () {
+                Navigator.pushNamed(context, '/RecordVideo');
+              }),
         ],
         backgroundColor: MyColors.white,
         elevation: 0.0, //para tirar a sombra
@@ -89,25 +214,33 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         controller: tabController,
         children: tabList(context),
       ),
-      bottomNavigationBar: BottomNavigationBar(        
+      bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (index) {
-
           setState(() {
             currentIndex = index;
           });
 
-            tabController.animateTo(currentIndex);
-
+          tabController.animateTo(currentIndex);
         },
         items: [
           BottomNavigationBarItem(
-            title: Text("Meus vídeos", style: TextStyle(color: currentIndex == 0 ? MyColors.primaryColor : MyColors.grey, fontSize: 18)),
+            title: Text("Meus vídeos",
+                style: TextStyle(
+                    color: currentIndex == 0
+                        ? MyColors.primaryColor
+                        : MyColors.grey,
+                    fontSize: 18)),
             icon: Icon(Icons.video_library, color: MyColors.grey),
             activeIcon: Icon(Icons.video_library, color: MyColors.primaryColor),
           ),
           BottomNavigationBarItem(
-            title: Text("Configurações", style: TextStyle(color: currentIndex == 1 ? MyColors.primaryColor : MyColors.grey, fontSize: 18)),
+            title: Text("Configurações",
+                style: TextStyle(
+                    color: currentIndex == 1
+                        ? MyColors.primaryColor
+                        : MyColors.grey,
+                    fontSize: 18)),
             icon: Icon(Icons.settings, color: MyColors.grey),
             activeIcon: Icon(Icons.settings, color: MyColors.primaryColor),
           )
