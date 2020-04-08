ad01a6d1d1d9cb9ef919c12b2632455280463296
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-08 15:48:30 -0400

    Adequando a api PublicApi para seguir o padrão das outras apis

--- projeto_experimental/lib/src/domain/public/api/PublicApi.dart
+++ projeto_experimental/lib/src/domain/public/api/PublicApi.dart
@@ -3,11 +3,9 @@ import 'dart:convert';
 import '../../../HttpConfig.dart';
 
 class PublicApi extends HttpConfig {
-  login(userName, userPassword) => this.http.post(
-        "/testes/smartemotion/app/api/users/auth.php",
-        queryParameters: {
-          "usuario": userName.toString(),
-          "senha": userPassword.toString()
-        },
-      );
+  login(userName, userPassword) => 
+    this.http.post("/testes/smartemotion/app/api/users/auth.php", data: {
+      "usuario": userName.toString(),
+      "senha": userPassword.toString()
+    });
 }
