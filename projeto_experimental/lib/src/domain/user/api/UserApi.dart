1fd6cb028077db1b8800f0e3d29f543cd6c99a1d
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-09 10:02:02 -0400

    Adicionadas funções para se conectar com a API e pegar Ip e localização do usuário na Api do domínio Users

--- projeto_experimental/lib/src/domain/user/api/UserApi.dart
+++ projeto_experimental/lib/src/domain/user/api/UserApi.dart
@@ -3,5 +3,16 @@ import 'dart:convert';
 import '../../../HttpConfig.dart';
 
 class UserApi extends HttpConfig {
-  
+  fetchLocation(userId, lat, lng) => 
+    this.http.post("/testes/smartemotion/app/api/users/fetchLocation.php", data: { 
+      "token": "token", 
+      "id_usuario": userId, 
+      "lat": lat, 
+      "lng": lng 
+    });
+  getIp(userId) => 
+    this.http.post("/testes/smartemotion/app/api/users/getIp.php", data: { 
+      "token": "token", 
+      "id_usuario": userId      
+    });
 }
