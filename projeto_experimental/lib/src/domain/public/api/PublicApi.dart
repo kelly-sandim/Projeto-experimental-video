849424d00a76e448578ffa1612b92fd06f6b31aa
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-09 16:22:35 -0400

    Criada função para se comunicar com a API e enviar os dados do cadastro do usuário

--- projeto_experimental/lib/src/domain/public/api/PublicApi.dart
+++ projeto_experimental/lib/src/domain/public/api/PublicApi.dart
@@ -9,4 +9,15 @@ class PublicApi extends HttpConfig {
       "email": controllerEmail.text,
       "senha": controllerPassword.text
     });
+  register(controllerName, controllerEmail, controllerPassword, controllerBirthday, controllerPhone, controllerCountry, controllerIdentity) =>
+    this.http.post("/testes/smartemotion/app/api/public/register.php", data: {
+      "token": "token",
+      "nome": controllerName.text,
+      "email": controllerEmail.text,
+      "senha": controllerPassword.text,
+      "data_aniversario": controllerBirthday.text,
+      "telefone": controllerPhone.text,
+      "pais": controllerCountry.text,
+      "identidade": controllerIdentity.text
+    });
 }
