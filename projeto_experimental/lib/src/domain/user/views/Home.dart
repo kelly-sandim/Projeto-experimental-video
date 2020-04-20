f4757623c308bf8e1414d94277d66c5acf5951f8
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-20 11:10:48 -0400

    Página Home agora mostra o título do vídeo, a data da postagem do mesmo e a média das emoções, ao invés do vídeo em si

--- projeto_experimental/lib/src/domain/user/views/Home.dart
+++ projeto_experimental/lib/src/domain/user/views/Home.dart
@@ -12,6 +12,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -20,11 +22,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   int currentIndex = 0;
-  TabController tabController;
-  List<VideoPlayerController> controllerVideoList =
-      new List<VideoPlayerController>();
-  List<String> titleVideoList = 
-      new List<String>();
+  TabController tabController; 
+
+  var jsonData;
+ 
   String messageError;
   bool isLoading = true;
   var userId;
@@ -46,10 +47,15 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   child: new ListView.builder(
                       shrinkWrap: true,
                       physics: BouncingScrollPhysics(),
-                      itemCount: controllerVideoList == null
-                          ? 0
-                          : controllerVideoList.length,
+                      itemCount: jsonData == null ? 0 : jsonData.length,
                       itemBuilder: (BuildContext context, int index) {
+                        var videoId = jsonData[index]['Id do Video'];
+                        var videoName = jsonData[index]['Nome do Video'];
+                        var videoDate = jsonData[index]['Data do Video'];
+                        var mediaVoice = jsonData[index]['Media Voz'];
+                        var mediaImage = jsonData[index]['Media Imagem'];
+                        var mediaText = jsonData[index]['Media Texto'];
+                        
                         return Card(
                           child: Column(
                             children: <Widget>[
@@ -58,29 +64,37 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                                   new ListTile(
                                     leading: Icon(Icons.play_circle_filled,
                                         color: MyColors.grey, size: 30),
-                                    title: Text(titleVideoList[index],
-                                        style: TextStyle(
-                                            color: MyColors.grey,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20)),
+                                    title: Text("$videoName\n", style: TextStyle(
+                                                  fontSize: 20, 
+                                                  color: MyColors.grey,
+                                                  fontWeight: FontWeight.bold, 
+                                                ),),
+                                    subtitle: RichText(
+                                        text: TextSpan(
+                                          text: "Postado em: $videoDate\n\n",
+                                          style: TextStyle(
+                                                    fontSize: 20, 
+                                                    color: MyColors.grey,
+                                                    fontWeight: FontWeight.bold, 
+                                                  ),
+                                          children: [
+                                            TextSpan(text: "Média das Vozes: ", style: TextStyle(fontSize: 15, color: MyColors.grey, fontWeight: FontWeight.bold)),
+                                            WidgetSpan(child: mediaVoice),
+                                            TextSpan(text: "\n"),
+                                            TextSpan(text: "Média das Imagens: ", style: TextStyle(fontSize: 15, color: MyColors.grey, fontWeight: FontWeight.bold)),
+                                            WidgetSpan(child: mediaImage),
+                                            TextSpan(text: "\n"),
+                                            TextSpan(text: "Média dos Textos: ", style: TextStyle(fontSize: 15, color: MyColors.grey, fontWeight: FontWeight.bold)),
+                                            WidgetSpan(child: mediaText),                                            
+                                            TextSpan(text: "\n"),
+                                          ],
+                                        ),
+                                      ), 
                                     onTap: () {
-                                      Navigator.pushNamed(context, '/Result');
+                                      _setDataToResultPage(videoId, _callResultPage);
                                     },
                                   ),
-                                  AspectRatio(
-                                  aspectRatio: controllerVideoList[index].value.aspectRatio, 
-                                  child: Stack(
-                                      alignment: FractionalOffset
-                                              .bottomRight +
-                                          const FractionalOffset(-0.1, -0.1),
-                                      children: <Widget>[
-                                        VideoPlayer(
-                                            controllerVideoList[index]),
-                                        _PlayPauseOverlay(controller: controllerVideoList[index]),
-                                        VideoProgressIndicator(controllerVideoList[index], allowScrubbing: true),
-                                      ]
-                                    ),
-                                  ),
+                                  
                                 ],
                               )
                             ],
@@ -119,6 +133,21 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     return _tabList;
   }
 
+  _setDataToResultPage(var videoId, _callResultPage) async
+  {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userId = prefs.getString('userId');
+    });
+
+    _callResultPage();
+  }
+
+  _callResultPage()
+  {
+    Navigator.pushNamed(context, '/Result');
+  }
+
   _loadData(_loadVideoList) async
   {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -129,7 +158,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   }
 
   _loadVideoList(var userId) async {    
-    var responseGet = await new VideoApi().getVideos(userId);
+    var responseGet = await new VideoApi().getAllVideoData(userId);
     var response = jsonDecode(responseGet.toString());
 
     print(response);
@@ -140,20 +169,65 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         _showDialog();
       });
     } else {
-      for (var item in response["videos"]) {
-        controllerVideoList.add(new VideoPlayerController.network(item));
-        var urlSplitted = item.split("/");
-        titleVideoList.add(urlSplitted[urlSplitted.length-1]);
-      }
+        var dataD;
+        var dataE; 
+        var dataF; 
+        var jsonEntry = new List();
 
-      for (var item in controllerVideoList) {
-        item.addListener(() {
-          setState(() {});
-        });
-        item.setLooping(false);
-        item.initialize().then((_) => setState(() {}));          
-      }
-    }
+        for (var item in response['data']) {
+          var videoData = {};
+          var dataA = item['id'];
+          DateTime dateTime = DateFormat("yyyy-MM-dd hh:mm").parse(item['date_created']);
+          var dataB = "${dateTime.day}/${dateTime.month}/${dateTime.year} - ${dateTime.hour}:${dateTime.minute}";        
+          var dataC = item['nome'];
+          var mediaVoice = item['medias_voice'];
+          if((mediaVoice) < 0.33)
+          {
+            dataD = Image.asset('./lib/src/assets/images/frowning-face.png', width: 35, height: 35);
+          }
+          else if((mediaVoice) >= 0.33 && (mediaVoice) < 0.6)
+          {
+            dataD = Image.asset('./lib/src/assets/images/neutral-face.png', width: 35, height: 35);
+          }
+          else
+          {
+            dataD = Image.asset('./lib/src/assets/images/smiling-face.png', width: 35, height: 35);
+          }
+
+          var mediaImage = item['medias_image'];        
+          if((mediaImage) < 0.33)
+          {
+            dataE = Image.asset('./lib/src/assets/images/frowning-face.png', width: 35, height: 35);
+          }
+          else if((mediaImage) >= 0.33 && (mediaImage) < 0.6)
+          {
+            dataE = Image.asset('./lib/src/assets/images/neutral-face.png', width: 35, height: 35);
+          }
+          else
+          {
+            dataE = Image.asset('./lib/src/assets/images/smiling-face.png', width: 35, height: 35);
+          }
+
+          var mediaText = item['medias_text'];        
+          if((mediaText) < 0.33)
+          {
+            dataF = Image.asset('./lib/src/assets/images/frowning-face.png', width: 35, height: 35);
+          }
+          else if((mediaText) >= 0.33 && (mediaText) < 0.6)
+          {
+            dataF = Image.asset('./lib/src/assets/images/neutral-face.png', width: 35, height: 35);
+          }
+          else
+          {
+            dataF = Image.asset('./lib/src/assets/images/smiling-face.png', width: 35, height: 35);
+          }
+
+          videoData.addAll({'Id do Video':dataA, 'Data do Video':dataB, 'Nome do Video':dataC, 'Media Voz':dataD, 'Media Imagem':dataE, 'Media Texto':dataF});
+          jsonEntry.add(videoData);
+        }//fim for
+
+        jsonData = jsonEntry;
+    }//fim else
     setState(() {
       isLoading = false;
     });
@@ -185,7 +259,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
-    ]);
+    ]);    
     tabController = TabController(vsync: this, length: 2);
     _loadData(_loadVideoList);
   }
@@ -193,9 +267,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   @override
   void dispose() {
     tabController.dispose();
-    for (var item in controllerVideoList) {
-      item.dispose();          
-    }
+    
     super.dispose();
   }
 
